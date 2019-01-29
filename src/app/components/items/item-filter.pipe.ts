7df7660dd5363876acadb601efe88dbc84cc6a7e
TomasGir <gircius.t@gmail.com>
2019-01-29 16:38:20 +0200

    Added filter by equipment category.

--- src/app/components/items/item-filter.pipe.ts
+++ src/app/components/items/item-filter.pipe.ts
@@ -10,7 +10,16 @@ export class ItemFilterPipe implements PipeTransform {
     if (!items || !searchTerm) {
       return items;
     }
-    return items.filter(item =>
+    const nameFilter = items.filter(item =>
       item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
+    const categoryFilter = items.filter(item =>
+      item.equipment_category.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
+    return uniqueArray(nameFilter.concat(categoryFilter));
   }
 }
+
+const uniqueArray = function (arrArg) {
+  return arrArg.filter(function (elem, pos, arr) {
+    return arr.indexOf(elem) === pos;
+  });
+};
